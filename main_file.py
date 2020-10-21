80b1e06e117c48d4b39e67a15df4d2f870af20fb
Umer Jamil <umerjamil@protonmail.com>
2020-10-21 21:00:21 +0500

    first commit

---
+++ main_file.py
@@ -0,0 +1,36 @@
+import gym
+import torch as T
+import numpy as np
+
+from agent import Agent
+from neural_network import LinearDeepQNetwork
+
+if __name__ == '__main__':
+    env = gym.make('CartPole-v1')
+    n_games = 10000
+    scores = []
+    eps_history = []
+
+    agent = Agent(lr=0.0001, input_dims=env.observation_space.shape, \
+                    n_actions = env.action_space.n)
+
+    for i in range(n_games):
+        score = 0
+        done = False
+        obs = env.reset()
+
+        while not done:
+            action = agent.choose_action(obs)
+            obs_, reward, done, info = env.step(action)
+            score += reward
+            agent.learn(obs, action, reward, obs_)
+            obs = obs_
+        scores.append(score)
+        eps_history.append(agent.epsilon)
+        if i % 100 == 0:
+            avg_score = np.mean(scores[-100:])
+            print('episode ', i, 'score %.1f avg score %.1f epsilon %.2f' %
+                  (score, avg_score, agent.epsilon))
+    filename = 'cartpole_naive_dqn.png'
+    x = [i+1 for i in range(n_games)]
+    plot_learning_curve(x, scores, eps_history, filename)
---
+++ readme.md
@@ -0,0 +1,6 @@
+## Deep Q Learning - Naive Implementation
+
+- main_file.py: Agent play implementation in OpenAI Gym Cart Pole Environment
+- neural_network.py: Neural Network Implementation in PyTorch
+- agent.py: Agent code implementation
+- utils.py: Contains functions for plotting
